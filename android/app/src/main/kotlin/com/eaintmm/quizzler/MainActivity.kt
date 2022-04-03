fd62def5f2663dc319e7768bc52cd025e3937b32
EaintMM <eaintmyintmon@gmail.com>
2022-04-03 22:15:45 +0630

    Initial commit

---
+++ android/app/src/main/kotlin/com/eaintmm/quizzler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eaintmm.quizzler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  String? question;
+  bool? answer;
+
+/*   Question(q, a) {
+    question = q;
+    answer = a;
+  } */
+
+  Question(this.question, this.answer);
+}
